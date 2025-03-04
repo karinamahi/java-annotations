a55924d701b96b6929936e633927008b260544be
karinamahi <karinahiratabueno@gmail.com>
2025-03-04 15:35:22 -0300

    ✨ basic annotation

---
+++ src/main/java/com/khirata/App.java
@@ -0,0 +1,43 @@
+package com.khirata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class App 
+{
+    public static void main( String[] args ) throws InvocationTargetException, IllegalAccessException {
+        var object = new MyClass();
+        new Processor().process(object);
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MyAnnotation {
+}
+
+class Processor {
+    public void process(Object object) throws InvocationTargetException, IllegalAccessException {
+        var methods = object.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyAnnotation.class)) {
+                System.out.println("Executing MyAnnotation before executing the method " + method.getName()); //it will be executed before the annotated method
+            }
+            method.invoke(object);
+        }
+    }
+}
+class MyClass {
+    @MyAnnotation
+    public void annotatedMethod() {
+        System.out.println("Executing annotatedMethod..");
+    }
+
+    public void nonAnnotatedMethod() {
+        System.out.println("Executing nonAnnotatedMethod..");
+    }
+}
